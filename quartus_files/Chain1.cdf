7316e1bc4c41557820eb2666c9b86b61f254c5cb
dayadim <amaanahmed_@outlook.com>
2023-11-26 16:25:48 -0800

    fixed buttons
    LCD works i think. did something with this code:
    
    volatile char * LCD_display_ptr = (char *) 0xFF203050; // 16x2 character display
    int x, y;
    char * text_ptr;
    char instruction;
    . . .
    instruction = x;
    if (y != 0)
    instruction |= 0x40; // set bit 6 for bottom row
    instruction |= 0x80; // need to set bit 7 to set the cursor location
    *(LCD_display_ptr) = instruction; // write to the LCD instruction register
    while ( *(text_ptr) )
    {
    *(LCD_display_ptr + 1) = *(text_ptr); // write to the LCD Data register
    ++text_ptr;
    }
    Figure 33. An example of code that uses the LCD display port.
    
    buttons work (needed ~ after all) (renamed to KEYS so use KEYS_BASE)
    audio is not working, but i've yet to do something with that other than record and play.

---
+++ quartus_files/Chain1.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 16.1.0 Build 196 10/24/2016 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(EP4CE115F29) Path("C:/eclipseFiles/178project/178-project/quartus files 3/output_files/") File("project_system_time_limited.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
---
+++ quartus_files/software/testing/obj/default/hello_world.d
@@ -0,0 +1,20 @@
+obj/default/hello_world.o: hello_world.c \
+ ../bsp_testing//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h \
+ ../bsp_testing//HAL/inc/alt_types.h \
+ ../bsp_testing//HAL/inc/sys/alt_dev.h ../bsp_testing/system.h \
+ ../bsp_testing/linker.h ../bsp_testing//HAL/inc/sys/alt_llist.h \
+ ../bsp_testing//HAL/inc/priv/alt_dev_llist.h
+
+../bsp_testing//drivers/inc/Altera_UP_SD_Card_Avalon_Interface.h:
+
+../bsp_testing//HAL/inc/alt_types.h:
+
+../bsp_testing//HAL/inc/sys/alt_dev.h:
+
+../bsp_testing/system.h:
+
+../bsp_testing/linker.h:
+
+../bsp_testing//HAL/inc/sys/alt_llist.h:
+
+../bsp_testing//HAL/inc/priv/alt_dev_llist.h:
